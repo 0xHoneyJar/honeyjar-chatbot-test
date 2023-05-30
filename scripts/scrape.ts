e7459c6847e12c6ba0f1e8b133f196a2de536b50
treblale <albertphongle@gmail.com>
2023-05-29 22:53:36 -0700

    Update scrape.ts

--- scripts/scrape.ts
+++ scripts/scrape.ts
@@ -4,32 +4,44 @@ import * as cheerio from "cheerio";
 import fs from "fs";
 import { encode } from "gpt-3-encoder";
 
-const BASE_URL = "http://www.paulgraham.com/";
+const BASE_URL = "https://0xhoneyjar.mirror.xyz/";
 const CHUNK_SIZE = 200;
 
 const getLinks = async () => {
-  const html = await axios.get(`${BASE_URL}articles.html`);
+  const html = await axios.get(`${BASE_URL}`);
   const $ = cheerio.load(html.data);
-  const tables = $("table");
 
   const linksArr: { url: string; title: string }[] = [];
 
-  tables.each((i, table) => {
-    if (i === 2) {
-      const links = $(table).find("a");
-      links.each((i, link) => {
-        const url = $(link).attr("href");
-        const title = $(link).text();
+  // start from the body
+  let body = $("body");
 
-        if (url && url.endsWith(".html")) {
-          const linkObj = {
-            url,
-            title
-          };
+  // target the first div
+  let firstDiv = body.children("div").first();
 
-          linksArr.push(linkObj);
-        }
-      });
+  // target the second div within the first div
+  let secondDiv = firstDiv.children("div").eq(1);
+
+  // target the first div within the second div
+  let nestedFirstDiv = secondDiv.children("div").first();
+
+  // target the fifth div within the nested first div
+  let finalDiv = nestedFirstDiv.children("div").eq(4);
+
+  // find the a tags within the final div
+  let links = finalDiv.find("a");
+
+  links.each((i, link) => {
+    const url = $(link).attr("href");
+    const title = $(link).text();
+
+    if (url) {
+      const linkObj = {
+        url,
+        title,
+      };
+
+      linksArr.push(linkObj);
     }
   });
 
@@ -47,15 +59,15 @@ const getEssay = async (linkObj: { url: string; title: string }) => {
     content: "",
     length: 0,
     tokens: 0,
-    chunks: []
+    chunks: [],
   };
 
   const fullLink = BASE_URL + url;
   const html = await axios.get(fullLink);
   const $ = cheerio.load(html.data);
-  const tables = $("table");
+  const div = $("table");
 
-  tables.each((i, table) => {
+  div.each((i, table) => {
     if (i === 1) {
       const text = $(table).text();
 
@@ -80,7 +92,9 @@ const getEssay = async (linkObj: { url: string; title: string }) => {
       if (lastSentence && lastSentence.includes("Thanks to")) {
         const thanksToSplit = lastSentence.split("Thanks to");
 
-        if (thanksToSplit[1].trim()[thanksToSplit[1].trim().length - 1] === ".") {
+        if (
+          thanksToSplit[1].trim()[thanksToSplit[1].trim().length - 1] === "."
+        ) {
           thanksTo = "Thanks to " + thanksToSplit[1].trim();
         } else {
           thanksTo = "Thanks to " + thanksToSplit[1].trim() + ".";
@@ -99,7 +113,7 @@ const getEssay = async (linkObj: { url: string; title: string }) => {
         content: trimmedContent,
         length: trimmedContent.length,
         tokens: encode(trimmedContent).length,
-        chunks: []
+        chunks: [],
       };
     }
   });
@@ -107,96 +121,98 @@ const getEssay = async (linkObj: { url: string; title: string }) => {
   return essay;
 };
 
-const chunkEssay = async (essay: PGEssay) => {
-  const { title, url, date, thanks, content, ...chunklessSection } = essay;
+// const chunkEssay = async (essay: PGEssay) => {
+//   const { title, url, date, thanks, content, ...chunklessSection } = essay;
 
-  let essayTextChunks = [];
+//   let essayTextChunks = [];
 
-  if (encode(content).length > CHUNK_SIZE) {
-    const split = content.split(". ");
-    let chunkText = "";
+//   if (encode(content).length > CHUNK_SIZE) {
+//     const split = content.split(". ");
+//     let chunkText = "";
 
-    for (let i = 0; i < split.length; i++) {
-      const sentence = split[i];
-      const sentenceTokenLength = encode(sentence);
-      const chunkTextTokenLength = encode(chunkText).length;
+//     for (let i = 0; i < split.length; i++) {
+//       const sentence = split[i];
+//       const sentenceTokenLength = encode(sentence);
+//       const chunkTextTokenLength = encode(chunkText).length;
 
-      if (chunkTextTokenLength + sentenceTokenLength.length > CHUNK_SIZE) {
-        essayTextChunks.push(chunkText);
-        chunkText = "";
-      }
+//       if (chunkTextTokenLength + sentenceTokenLength.length > CHUNK_SIZE) {
+//         essayTextChunks.push(chunkText);
+//         chunkText = "";
+//       }
 
-      if (sentence[sentence.length - 1].match(/[a-z0-9]/i)) {
-        chunkText += sentence + ". ";
-      } else {
-        chunkText += sentence + " ";
-      }
-    }
+//       if (sentence[sentence.length - 1].match(/[a-z0-9]/i)) {
+//         chunkText += sentence + ". ";
+//       } else {
+//         chunkText += sentence + " ";
+//       }
+//     }
 
-    essayTextChunks.push(chunkText.trim());
-  } else {
-    essayTextChunks.push(content.trim());
-  }
+//     essayTextChunks.push(chunkText.trim());
+//   } else {
+//     essayTextChunks.push(content.trim());
+//   }
 
-  const essayChunks = essayTextChunks.map((text) => {
-    const trimmedText = text.trim();
+//   const essayChunks = essayTextChunks.map((text) => {
+//     const trimmedText = text.trim();
 
-    const chunk: PGChunk = {
-      essay_title: title,
-      essay_url: url,
-      essay_date: date,
-      essay_thanks: thanks,
-      content: trimmedText,
-      content_length: trimmedText.length,
-      content_tokens: encode(trimmedText).length,
-      embedding: []
-    };
+//     const chunk: PGChunk = {
+//       essay_title: title,
+//       essay_url: url,
+//       essay_date: date,
+//       essay_thanks: thanks,
+//       content: trimmedText,
+//       content_length: trimmedText.length,
+//       content_tokens: encode(trimmedText).length,
+//       embedding: [],
+//     };
 
-    return chunk;
-  });
+//     return chunk;
+//   });
 
-  if (essayChunks.length > 1) {
-    for (let i = 0; i < essayChunks.length; i++) {
-      const chunk = essayChunks[i];
-      const prevChunk = essayChunks[i - 1];
+//   if (essayChunks.length > 1) {
+//     for (let i = 0; i < essayChunks.length; i++) {
+//       const chunk = essayChunks[i];
+//       const prevChunk = essayChunks[i - 1];
 
-      if (chunk.content_tokens < 100 && prevChunk) {
-        prevChunk.content += " " + chunk.content;
-        prevChunk.content_length += chunk.content_length;
-        prevChunk.content_tokens += chunk.content_tokens;
-        essayChunks.splice(i, 1);
-        i--;
-      }
-    }
-  }
+//       if (chunk.content_tokens < 100 && prevChunk) {
+//         prevChunk.content += " " + chunk.content;
+//         prevChunk.content_length += chunk.content_length;
+//         prevChunk.content_tokens += chunk.content_tokens;
+//         essayChunks.splice(i, 1);
+//         i--;
+//       }
+//     }
+//   }
 
-  const chunkedSection: PGEssay = {
-    ...essay,
-    chunks: essayChunks
-  };
+//   const chunkedSection: PGEssay = {
+//     ...essay,
+//     chunks: essayChunks,
+//   };
 
-  return chunkedSection;
-};
+//   return chunkedSection;
+// };
 
 (async () => {
   const links = await getLinks();
+  console.log(links);
+  console.log(links.length);
 
-  let essays = [];
+  // let essays = [];
 
-  for (let i = 0; i < links.length; i++) {
-    const essay = await getEssay(links[i]);
-    const chunkedEssay = await chunkEssay(essay);
-    essays.push(chunkedEssay);
-  }
+  // for (let i = 0; i < links.length; i++) {
+  //   const essay = await getEssay(links[i]);
+  //   const chunkedEssay = await chunkEssay(essay);
+  //   essays.push(chunkedEssay);
+  // }
 
-  const json: PGJSON = {
-    current_date: "2023-03-01",
-    author: "Paul Graham",
-    url: "http://www.paulgraham.com/articles.html",
-    length: essays.reduce((acc, essay) => acc + essay.length, 0),
-    tokens: essays.reduce((acc, essay) => acc + essay.tokens, 0),
-    essays
-  };
+  // const json: PGJSON = {
+  //   current_date: "2023-03-01",
+  //   author: "Paul Graham",
+  //   url: "http://www.paulgraham.com/articles.html",
+  //   length: essays.reduce((acc, essay) => acc + essay.length, 0),
+  //   tokens: essays.reduce((acc, essay) => acc + essay.tokens, 0),
+  //   essays,
+  // };
 
-  fs.writeFileSync("scripts/pg.json", JSON.stringify(json));
+  // fs.writeFileSync("scripts/pg.json", JSON.stringify(json));
 })();
